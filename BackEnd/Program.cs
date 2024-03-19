5ed1071490bbbee363855c01443f56599a999079
ravennmon <francyneeleocadio@gmail.com>
2024-03-18 21:23:11 -0300

    Criando a lista

---
+++ BackEnd/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<Produto> produto = new List<Produto>(){
+
+    new Produto("Celular", "Android"),
+    new Produto("Notebook", "Dell"),
+    new Produto("Garrafa", "Stanley"),
+
+};
+
+//End Point = Funcionalidades
+
+app.MapGet("/", () => "Projeto A de backend!");
+
+app.MapGet("/api/produto", () => produto);
+
+app.Run();
+
+public record Produto(string Nome, string Decricao);
+
+
